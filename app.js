914957f956afcaa7a60e50a2ff3f32cd5bdff482
dmattt98 <dmattt98@gmail.com>
2013-12-01 16:00:34 -0600

    0.0.06
    Switched over to new file storage system and version support completely

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var libs = require(__dirname + '/libraries.json'),    // gets the json file wher
 	save = require('./save.js'),    // gets the save.js  module
 	colors = require('colors');    // gets the colors module which is used to add color to the console
 
-save.files(libs, 'latest');    // calls the files function in save.js which saves locally any undownloaded files
+save.files(libs);    // calls the files function in save.js which saves locally any undownloaded files
 
 app.get('/one/*', function(req, res) {    // express routing to everything in /one/ 
 
@@ -17,19 +17,27 @@ app.get('/one/*', function(req, res) {    // express routing to everything in /o
 	console.log('rqst: '.green + 'a request for \'' + req.params[0].yellow + '\' was made');    // logs to the console the url requested
 
 	var requested = req.params[0].split("/"),    // splits the request into individual library names
-		urls = [];    // creates an empty url array to be used later
+		libraries = [],
+		versions = [],
+		paths = [];
 
 	for(var i = 0; i < requested.length; i++) {  // a for loop to fill the urls array with the urls of the requested libraries
-		if(typeof libs[requested[i].replace('.js', '')] !== 'undefined')    // makes sure the library actually exists
-			urls.push(libs[requested[i].replace('.js', '')]);
+		if(typeof requested[i].replace('.js', '').split('-')[0] !== 'undefined') {    // makes sure the library actually exists
+			var rqst = requested[i].replace('.js', '').split('-');
+			libraries.push(rqst[0]);
+			if(typeof rqst[1] != 'undefined')
+				versions.push(rqst[1]);
+			else
+				versions.push('latest');
+		}
 	}
 
-	async.map(urls, , function(error, result) {    // async's map function gets the results from the request.get function and puts it into one array
-		var data = '';    // creates a blank string to store all the libraries in
-		for(var i = 0; i < result.length; i++) {    // for loop to get the library's code from each json object in the results of async.map
-			data += result[i].body + ' ';    // appends the library's code to the datastring
-		}
-		res.end(data);    // server the browser the appended libraries
+	for(var i = 0; i < libraries.length; i++) {
+		paths.push('./libraries/' + libraries[i] + '/' + versions[i] + '.js');
+	}
+
+	async.map(paths, fs.readFile, function(error, result) {    // async's map function gets the results from the request.get function and puts it into one array
+		res.end(result.join('\n'));   // server the browser the appended libraries
 	});
 });
 
