3695741664ccfa7d340fa326a5dc1e3184a3c99a
dmattt98 <dmattt98@gmail.com>
2013-12-01 12:40:04 -0600

    0.0.04
    Successfully joins multiple javascript files based on url

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-var libs = require(__dirname + '/libraries.json'),    // get json file where all libraries are stored
-	express = require('express'),
-	app = express(),
-	request = require('request'),
-	mime = require('mime');
+var libs = require(__dirname + '/libraries.json'),    // gets the json file where all libraries are stored
+	fs = require('fs');    // gets the filesystem module
+	express = require('express'),    // gets express
+	app = express(),     // creates the express app
+	request = require('request'),    // gets request
+	async = require('async');    // gets async
 
-app.get('/one/*', function(req, res) {
+app.get('/one/*', function(req, res) {    // express routing to everything in /one/ 
 
-	console.log('a request for ' + req.params[0] + ' was made');
+	res.set('Content-Type', 'application/javascript');    // sets the file type in the header
+
+	console.log('a request for ' + req.params[0] + ' was made');    // logs to the console the url requested
 
 	var requested = req.params[0].split("/"),    // splits the request into individual library names
-		urls = '';
+		urls = [];    // creates an empty url array to be used later
 
-	for(var i = 0; i < requested.length; i++) {    // for loop to add the library url to urls separated by <br> tags
-		if (typeof libs[requested[i].replace('.js', '')] !== 'undefined')    // checks to make sure the library is listed
-			urls += (libs[requested[i].replace('.js', '')] + ' ');
+	for(var i = 0; i < requested.length; i++) {  // a for loop to fill the urls array with the urls of the requested libraries
+		if(typeof libs[requested[i].replace('.js', '')] !== 'undefined')    // makes sure the library actually exists
+			urls.push(libs[requested[i].replace('.js', '')]);
 	}
 
-	res.status(200);
-	res.format({
-		'application/javascript': function() {
-			res.send(urls);
+	async.map(urls, request.get, function(error, result) {    // async's map function gets the results from the request.get function and puts it into one array
+		var data = '';    // creates a blank string to store all the libraries in
+		for(var i = 0; i < result.length; i++) {    // for loop to get the library's code from each json objects in the results of async.map
+			data += result[i].body + ' ';    // appends the library's code to the datastring
 		}
+		res.end(data);    // server the browser the appended libraries
 	});
-	console.log(res.get('Content-Type'));
-	res.end();
 });
 
 app.listen(3000);
