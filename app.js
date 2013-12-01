c4e5757c6fc751ff6a5b2e2020adcfa00c82e403
dmattt98 <dmattt98@gmail.com>
2013-12-01 06:45:25 -0600

    0.0.01b
    more comment fixes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var libs = require(__dirname + '/libraries.json'),    // get json file where all libraries are stored
-	express = require('express'),    // requires express
-	app = express();    // creates an express object
+	express = require('express'),
+	app = express(),
+	request = require('request');
 
-app.get('/one/*', function(req, res) {    // used to get requests to /one/*
+app.get('/one/*', function(req, res) {
 
-	console.log('a request for ' + req.params[0] + ' was made');    // logs the request to the console
+	console.log('a request for ' + req.params[0] + ' was made');
 
 	var requested = req.params[0].split("/"),    // splits the request into individual library names
-		urls = '';    // creates a variable to be use to store the library locations
+		urls = '';
 
 	for(var i = 0; i < requested.length; i++) {    // for loop to add the library url to urls separated by <br> tags
 		urls += (libs[requested[i]] + '<br>');
@@ -16,5 +17,5 @@ app.get('/one/*', function(req, res) {    // used to get requests to /one/*
 	res.send(urls);    // sends the url string to the browser
 });
 
-app.listen(3000);    // sets express to listen on port 3000
-console.log('now listening to port 3000');    // logs that we are listen on port 3000
+app.listen(3000);
+console.log('now listening to port 3000');
